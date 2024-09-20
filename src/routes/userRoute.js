476ebd8087b30a0fa7feb28f74baf4320b9c4b93
wignn <tigfiragnafatur1933@gmail.com>
2024-09-20 12:40:11 +0700

    "Added getUserById function to userRoute and imported it from controller"

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,9 @@
 import { route } from "../config/express.js";
-import { getAllUsers, creatUser } from "../controller/route.js"; 
+import { getAllUsers, creatUser, getUserById } from "../controller/route.js"; 
 const userRoute = route;
 
 userRoute.get('/data', getAllUsers);
 userRoute.post('/create', creatUser);
+userRoute.get('/:id', getUserById);
 
 export { userRoute };
